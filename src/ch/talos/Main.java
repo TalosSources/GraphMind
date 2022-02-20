e834da4679de16298800c5e8e2aee77dbd441b62
Ismail Sahbane <ismail.sahbane@epfl.ch>
2022-02-20 19:26:07 +0100

    nothing hehe

--- src/ch/talos/Main.java
+++ src/ch/talos/Main.java
@@ -51,13 +51,6 @@ public class Main extends Application {
 //        mainWindow.setMaximized(true);
     }
 
-    public static void uglyDrawing() {
-        String ownJsonDirectory = HelperMethods.contentOfFile("ownSaveLocation");
-        Set<ModifiableNode> graph = JSONSaveManager.generateGraphFromJSON(ownJsonDirectory);
-        MutableGraphState mgs = new SimpleGraphState(graph);
-        DrawGraph.performDrawing(mgs, null);
-    }
-
     public static void simpleTests() {
         String externalJsonDirectory = HelperMethods.contentOfFile("externalSaveLocation");
         String ownJsonDirectory = HelperMethods.contentOfFile("ownSaveLocation");
@@ -86,50 +79,6 @@ public class Main extends Application {
          for(Node node : listGraph) {
          System.out.println(node.name() + " : " + node.degree());
          } /**/
-    }
-
-    public static void simpleGraphTest() {
-        Set<Node> graph = simpleGraph();
-
-        for (Node node : graph) System.out.print(node.textRepresentation());
-    }
-
-    public static Set<Node> simpleGraph() {
-        ModifiableNode Moi = new SimpleNode("Moi");
-        ModifiableNode ComputerScience = new SimpleNode("CompSci");
-        ModifiableNode EPFL = new SimpleNode("EPFL");
-        ModifiableNode MaterialScience = new SimpleNode("MaterialScience");
-        ModifiableNode BA3 = new SimpleNode("BA3");
-
-        String epflText =
-                "L'ecole polytechnique federale de Lausanne, ou je suis inscrit. Plutot pas mal dans l'ensemble.";
-        String MatSciText =
-                "La science qui etudie les materiaux, a priori aucun rapport avec moi.";
-
-        EPFL.setText(epflText);
-        MaterialScience.setText(MatSciText);
-
-        ComputerScience.setName("ComputerScience");
-
-        EPFL.setUrl("www.epfl.ch");
-
-        connectParentChild(Moi, EPFL);
-        connectParentChild(Moi, ComputerScience);
-        connectParentChild(EPFL, BA3);
-        connectSiblings(ComputerScience, EPFL);
-        connectSiblings(MaterialScience, EPFL);
-
-        return Set.of(Moi, ComputerScience, EPFL, MaterialScience, BA3);
-    }
-
-    public static void connectParentChild(ModifiableNode parent, ModifiableNode child) {
-        parent.addChild(child);
-        child.addParent(parent);
-    }
-
-    public static void connectSiblings(ModifiableNode s1, ModifiableNode s2) {
-        s1.addSibling(s2);
-        s2.addSibling(s1);
     }
 
     public static void writeTextRepresentation(Set<? extends Node> graph) {
